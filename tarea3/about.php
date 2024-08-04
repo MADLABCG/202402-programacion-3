2e599e84c986e02f81f9f2803d8716d2f9751529
Ivan Firestone <ivan.firestone@icloud.com>
2024-08-04 11:02:32 -0400

    Final

--- tarea3/about.php
+++ tarea3/about.php
@@ -7,30 +7,71 @@
         <p class="text-capitalize fw-bolder lh-lg text-primary fs-1 text">
             Ivan Firestone
         </p>
-        <div class="info_personal">
-            <div class="info">
-                Nombre: Ivan Firestone
-            </div>
-            <div class="info">
-                Matricula: 000900909-1
-            </div>
-            <div class="info">
-                Telefono: 809-555-5555
-            </div>
-            <div class="info">
-                Correo: ivan.firestone@icloud.com
-            </div>
-        </div>
+        <div class="row container-fluid">
+            <div class="col-6">
+                <div class="row">
+                    <div>
+                        <img src="/images/profile/profile.jpg" alt="Profile Image" class="img-fluid">
+                    </div>
+                    <div>
+                        <p>
+                            Director Técnico experimentado, ingeniero de software como pasión, con amplia práctica en
+                            ingeniería
+                            tecnológica e informática que busca obtener un puesto que permita la aplicación de
+                            habilidades
+                            de
+                            diseño, servicio al cliente y habilidades de soporte y pensamiento crítico para producir
+                            software
+                            que cumpla con todos los objetivos y objetivos trazados por el cliente.
+                        </p>
 
-        <div class="info_academica">
-            <div class="academica">
-                Universidad: Eugenio Maria de Hostos (UNIREMHOS)
+                        <p>
+                            Fullstack Software Engineer
+                            Java, Spring, Angular, Javascript, PHP, Laravel Framework, Desktop Apps
+                            SysAdmin
+                        </p>
+                    </div>
+                </div>
+
+                <div class="row">
+
+                    <div class="col-8">
+                        <a href="https://www.linkedin.com/in/ifirestone/" target="_blank" class="text-black me-4">
+                            <img src="/images/icons/linkedin.png" alt="Linkedin" class="img-fluid">
+                        </a>
+
+                    </div>
+
+                </div>
+
+
             </div>
-            <div class="academica">
-                Carrera: Licenciatura en Sistemas
-            </div>
-            <div class="academica">
-                Semestre: 8vo
+            <div class="col-6">
+                <div class="info_personal">
+                    <div class="info">
+                        Nombre: Ivan Firestone
+                    </div>
+                    <div class="info">
+                        Matricula: 000900909-1
+                    </div>
+                    <div class="info">
+                        Telefono: 809-555-5555
+                    </div>
+                    <div class="info">
+                        Correo: ivan.firestone@icloud.com
+                    </div>
+                </div>
+                <div class="info_academica">
+                    <div class="academica">
+                        Universidad: Eugenio Maria de Hostos (UNIREMHOS)
+                    </div>
+                    <div class="academica">
+                        Carrera: Licenciatura en Sistemas
+                    </div>
+                    <div class="academica">
+                        Semestre: 8vo
+                    </div>
+                </div>
             </div>
         </div>
     </div>
